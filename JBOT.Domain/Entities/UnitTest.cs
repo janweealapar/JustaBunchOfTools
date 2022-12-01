448bde3f65f62fe9b4af53a4045734ebe247edd9
Janwee Alapar <iamjanweealapar@gmail.com>
2022-12-02 00:30:43 +0800

    InitialCommit

---
+++ JBOT.Domain/Entities/UnitTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JBOT.Domain.Entities
+{
+    public class UnitTest
+    {
+        public int Id { get; set; }
+        public string TestName { get; set; }
+        public string Description { get; set; }
+        public string ObjectName { get; set; }
+        public string Arrange { get; set; }
+        public string Act { get; set; }
+        public bool Assert { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ JBOT/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JBOT.WPF.ViewModels
+{
+    public class MainWindowViewModel
+    {
+    }
+}
